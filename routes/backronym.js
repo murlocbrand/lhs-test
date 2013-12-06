2a2c3cd3736d81d771ba834f405c09591e8bdf5c
Axel Smeets <murlocbrand+git@gmail.com>
2013-12-06 09:31:23 +0100

    dunno if faster

--- routes/backronym.js
+++ routes/backronym.js
@@ -1,16 +1,11 @@
 exports.getBackronym = function(){
-	var words = ['Lunds',   'Hacker', '#space' ,
-				 'Large',   'Homo'  , 'Society',
-				 'Long' ,   'Happy' , 'Smiley' ,
-				 'Limited', 'Hyper' , 'Squid'  ];
+	var words_l = [ 'Lunds', 'Large', 'Limited', 'Lakrits', 'Latent'];
+	var words_h = [ 'Horticultural', 'Happy', 'Hacker', 'Horse'];
+	var words_s = [ 'Society', 'Squad', '#space', 'Smiley', 'Squid', 'Swarm'];
 
-	var rows = words.length / 3;
+	var l = words_l[Math.floor(Math.random() * words_l.length)];
+	var h = words_h[Math.floor(Math.random() * words_h.length)];
+	var s = words_s[Math.floor(Math.random() * words_s.length)];
 
-	var l = words[Math.floor(Math.random() * rows) * 3];
-	var h = words[Math.floor(Math.random() * rows) * 3 + 1];
-	var s = words[Math.floor(Math.random() * rows) * 3 + 2];
-
-	var lhs = l + " " + h + " " + s;
-
-	return lhs.replace(" #", "");
+	return (l + " " + h + " " + s).replace(" #", "");
 }
